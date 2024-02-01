637baddb49877dcaabefdf2379e5bd9326334b44
btwkenji <btwdesu@gmail.com>
2024-02-01 18:19:32 +0200

    chore: update project structure, create src/ and main.py

--- src/telegram.py
+++ src/telegram.py
@@ -8,12 +8,10 @@ bot = AsyncTeleBot('')
 async def send_welcome(message):
     await bot.reply_to(message, 'Hi, I\'m a TG Bot!')
 
-
 # Handle '/help'
 @bot.message_handler(commands=['help'])
 async def send_info(message):
     await bot.reply_to(message, "For assistance or feedback, DM me on Telegram: @lxudrr.")
-
 
 # Handle '/*' all unknown commands
 @bot.message_handler(func=lambda message: message.text.startswith('/'))
@@ -24,8 +22,6 @@ async def handle_unknown_commands(message):
 @bot.message_handler(func=lambda message: any(word in message.text.lower() for word in ['thanks', 'thx', 'thank']))
 async def handle_all_messages(message):
     await bot.reply_to(message, f'{message.from_user.id} + 1')
-
-
 
 if __name__ == '__main__':    
     asyncio.run(bot.polling())
