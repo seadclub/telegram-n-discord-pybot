312863d85525c3b7187991910165da76e584e867
Sasha <lxudrr@gmail.com>
2024-02-03 11:24:45 +0100

    feat: implement appreciating func

--- src/telegram.py
+++ src/telegram.py
@@ -27,9 +27,57 @@ async def handle_unknown_commands(message):
 
 
 # Handle 'appreciation'
-@bot.message_handler(func=lambda message: any(word in message.text.lower() for word in ['thanks', 'thx', 'thank']))
+@bot.message_handler(func=lambda message: any(word in message.text.lower() for word in ['thanks', 'thx', 'thank', 'дякую']))
 async def handle_all_messages(message):
-    await bot.reply_to(message, f'{message.from_user.id} + 1')
+    try:
+        message_sender = message.from_user
+        message_receiver = message.reply_to_message.from_user
+
+        # check whether message_sender doesn't reply to himself
+        if message_sender.id != message_receiver.id:
+            
+            # update db
+            async with aiosqlite.connect('users.sqlite3') as connection:
+                cursor = await connection.cursor()
+
+                # Fetch all user IDs from the database
+                await cursor.execute('SELECT * FROM users')
+                fetchall = await cursor.fetchall()
+
+
+                if message_sender.id in [user_id[1] for user_id in fetchall]:
+                    pass
+                else:
+                    # create message_sender's info
+                    await cursor.execute(f'INSERT INTO users (user_id, gratitudes) VALUES ("{message_sender.id}", 0)')
+
+                if message_receiver.id in [user_id[1] for user_id in fetchall]:
+                    # add gratitude points
+                    await cursor.execute(f'UPDATE users SET gratitudes = gratitudes + 1 WHERE user_id = {message_receiver.id}')
+                else:
+                    # create message_receiver's info
+                    await cursor.execute(f'INSERT INTO users (user_id, gratitudes) VALUES ("{message_receiver.id}", 1)')
+                
+                await connection.commit()
+                await cursor.close()
+
+
+            # show output 
+            async with aiosqlite.connect('users.sqlite3') as connection:
+                cursor = await connection.cursor()
+
+                await cursor.execute(f'SELECT gratitudes FROM users WHERE user_id = {message_sender.id}')
+                message_sender_gratitudes = (await cursor.fetchone())[0]
+
+                await cursor.execute(f'SELECT gratitudes FROM users WHERE user_id = {message_receiver.id}')
+                message_receiver_gratitudes = (await cursor.fetchone())[0]
+                
+                await bot.reply_to(message, f'<b>{message_sender.first_name}</b> ({message_sender_gratitudes}) respected to <b>{message_receiver.first_name}</b> ({message_receiver_gratitudes})', parse_mode='HTML')
+
+                await connection.commit()
+                await cursor.close()
+    except:
+        print('Unable to make a gratitude.')
 
 
 async def connection_to_db():
@@ -41,7 +89,7 @@ async def connection_to_db():
         # used to prepare table creation
         await cursor.execute('''CREATE TABLE IF NOT EXISTS users
                              (id integer PRIMARY KEY,
-                             name varchar(100),
+                             user_id integer,
                              gratitudes integer)''')
     
         # used to create a table
